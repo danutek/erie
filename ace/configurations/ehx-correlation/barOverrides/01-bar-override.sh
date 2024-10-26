f222f1b6393ce824619dcce3af4f14e83ad76c40
Daniel Cogswell <danutek@us.ibm.com>
2024-10-26 17:08:31 -0600

    Removed configurations

--- ace/configurations/ehx-correlation/barOverrides/01-bar-override.sh
+++
@@ -1,6 +0,0 @@
-./02-bar-override.properties.sh
-rm bar-override.yaml bar-override.properties.base64
-base64 bar-override.properties  > bar-override.properties.base64
-sed -i -e 's/^/    /' *.base64
-cat bar-override.yaml.template > bar-override.yaml
-cat bar-override.properties.base64 >> bar-override.yaml
--- ace/configurations/hlt-20-03/policies/01-policy.sh
+++
@@ -1,5 +0,0 @@
-rm policy.base64 policy-config.yaml
-base64 HLTPolicy.zip > policy.base64
-sed -i -e 's/^/    /' *.base64
-cat policy-config.yaml.template > policy-config.yaml
-cat policy.base64 >> policy-config.yaml
