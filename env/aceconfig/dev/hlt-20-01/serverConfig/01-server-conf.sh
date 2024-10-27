f5562717f471b7b754b5cbc0eda2d4ef748c738d
Daniel Cogswell <danutek@us.ibm.com>
2024-10-27 17:29:22 -0600

    configs

--- env/aceconfig/dev/hlt-20-01/serverConfig/01-server-conf.sh
+++ env/aceconfig/dev/hlt-20-01/serverConfig/01-server-conf.sh
@@ -1,5 +1,5 @@
 rm server.conf.yaml server.conf.base64
-base64 server.conf.txt  > server.conf.base64
+base64 -i server.conf.txt  > server.conf.base64
 sed -i -e 's/^/    /' *.base64
 cat server-conf.yaml.template > server.conf.yaml
 cat server.conf.base64 >> server.conf.yaml
