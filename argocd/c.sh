34f97869873d561e6bd9830d8257b4aec1ba26f0
Daniel Cogswell <danutek@us.ibm.com>
2024-10-26 19:45:30 -0600

    rearrange

--- argocd/c.sh
+++
@@ -1,2 +0,0 @@
-oc adm policy add-role-to-user admin system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller -n openshift-gitops
-argocd repo add https://github.com/danutek/erie.git
