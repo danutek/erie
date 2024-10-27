d30c1e6580716231f23996ac799d1584f00e63e5
danutek <36863617+danutek@users.noreply.github.com>
2024-10-27 17:41:00 -0600

    Delete env/aceconfig/dev/hlt-20-01 directory

--- env/aceconfig/dev/hlt-20-01/setdbparms/01-setdbparms.sh
+++
@@ -1,5 +0,0 @@
-rm setdbparms.base64 db-parms-config.yaml
-base64 -i setdbparms.txt > setdbparms.base64
-sed -i -e 's/^/    /' *.base64
-cat db-parms-config.yaml.template > db-parms-config.yaml
-cat setdbparms.base64 >> db-parms-config.yaml
